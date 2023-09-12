3e47fc26b19956b4ec4a89119d72989a809f68ff
AykutIpek <aykutipek_2001@hotmail.com>
2023-09-12 13:15:21 +0300

    Folders Created1

---
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Threads
+//
+//  Created by aykut ipek on 12.09.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
