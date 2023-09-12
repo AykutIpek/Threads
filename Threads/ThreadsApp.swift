e94bb4b073b4c69fc969c01255d4746590b86a3a
AykutIpek <aykutipek_2001@hotmail.com>
2023-09-12 12:58:48 +0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by aykut ipek on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
