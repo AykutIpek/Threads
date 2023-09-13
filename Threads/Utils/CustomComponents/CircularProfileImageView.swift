fa60fc6a206d0136f977719f81b999c6cf7cfbb0
AykutIpek <aykutipek_2001@hotmail.com>
2023-09-13 13:32:12 +0300

    Explore View UI added

---
+++ Threads/Utils/CustomComponents/CircularProfileImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProfileImageView.swift
+//  Threads
+//
+//  Created by aykut ipek on 13.09.2023.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircularProfileImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProfileImageView()
+    }
+}
---
+++ Threads/Utils/CustomComponents/UserCell.swift
@@ -0,0 +1,20 @@
+//
+//  UserCell.swift
+//  Threads
+//
+//  Created by aykut ipek on 13.09.2023.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCell()
+    }
+}
