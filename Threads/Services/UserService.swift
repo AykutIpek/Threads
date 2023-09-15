587dc99da845046e634d93c2eb00326d85a32aae
AykutIpek <aykutipek_2001@hotmail.com>
2023-09-15 14:59:12 +0300

    User service and data model written

---
+++ Threads/Services/UserService.swift
@@ -0,0 +1,31 @@
+//
+//  UserService.swift
+//  Threads
+//
+//  Created by aykut ipek on 15.09.2023.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+
+final class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    init() {
+        Task {
+            try await fetchCurrentUser()
+        }
+    }
+    
+    @MainActor
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+        print("DEBUG: User is \(user)")
+    }
+}
